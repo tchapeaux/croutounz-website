874ccb8b57b2123b5e7072536af31760216be37c
Thomas Chapeaux <thomas.chapeaux@big-captain.com>
2025-06-11 09:51:52 +0200

    dev: harmonize eslint config across projects

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    // configure html-self-closing for SFC
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+          normal: "never",
+        },
+      },
+    ],
+  },
+});
