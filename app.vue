0a89a49744ff078f24ec7bf542040b21d3b4288e
Thomas Chapeaux <thomas.chapeaux@big-captain.com>
2025-06-11 13:42:00 +0200

    Moar SEO tags

--- app.vue
+++ app.vue
@@ -5,3 +5,15 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script stup lang="ts">
+useSeoMeta({
+  title: "La Croutounz",
+  ogTitle: "La Croutounz",
+  description:
+    "Toutes les infos sur l'équipe d'improvisation belge de la Croutounz",
+  ogDescription:
+    "Toutes les infos sur l'équipe d'improvisation belge de la Croutounz",
+  ogImage: "/croutounz-avatar.png",
+});
+</script>
